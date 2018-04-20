642c45b87e387bd35a736b8f3e8f8cec6b6e04fc
Helen Strypko <elenkastrypko@gmail.com>
2018-04-20 20:17:26 +0300

    reworked the task correctly

--- hard/task3-2.js
+++ hard/task3-2.js
@@ -1,28 +1,15 @@
-var obj1 = {
-    name: "Helen",
-    surname: "Strypko"
-};
-
-function validate(obj1){
-    if(obj1.name && obj1.surname){
-        return true;
-    }else{
-        return false;
+function validate(arr,obj){
+   for(var i=0; i<arr.length; i++){
+       var key = arr[i]; 
+           if(!(obj.hasOwnProperty(key))) {            
+                throw new Error("'age' wasn't passed on object");
+           } 
+       }
+       return true;
     }
-}
-
-console.log(validate(obj1));
-
-function validate2(obj1){
-    if(obj1.name && obj1.surname && obj1.age){
-        return true;
-    }else{
-        console.error("error :'age' wasn't passed on object");
-    }
-}
-
-validate2(obj1);
 
 
+console.log(validate(["name","surname"],{name:"Roman",surname:"Rodomansky"}));
+console.log(validate(["name","surname","age"],{name:"Roman",surname:"Rodomansky"}));
 
 
