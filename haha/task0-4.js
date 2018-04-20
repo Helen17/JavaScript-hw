ae726fa93b5cc310580aaa8bafaaa40559a93213
Helen Strypko <elenkastrypko@gmail.com>
2018-04-20 18:46:49 +0300

    Add some changes

--- haha/task0-4.js
+++ haha/task0-4.js
@@ -1,5 +1,5 @@
-var name = prompt("Enter your name");
-var city = prompt("Enter your city");
-var date = prompt("Enter date of meeting");
+var name = prompt("Enter your name","Petro");
+var city = prompt("Enter your city","Milan");
+var date = prompt("Enter date of meeting","17:00");
 
-alert(name + " has a meeting " + date + " somewhere in " + city + " .");
+alert(name + " has a meeting at " + date + " somewhere in " + city + " .");
