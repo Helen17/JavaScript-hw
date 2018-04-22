8c709fa42228cc81fca604b7a8869e98fd209fd7
Helen Strypko <elenkastrypko@gmail.com>
2018-04-22 17:05:01 +0300

    Added Event Listener for button "start"

--- haha/task0-4.js
+++ haha/task0-4.js
@@ -1,5 +1,7 @@
+document.getElementById("start").addEventListener("click", function(){
 var name = prompt("Enter your name","Petro");
 var city = prompt("Enter your city","Milan");
 var date = prompt("Enter date of meeting","17:00");
 
 alert(name + " has a meeting at " + date + " somewhere in " + city + " .");
+});
