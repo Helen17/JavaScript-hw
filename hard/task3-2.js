0e91452c67d3840e7218529dae076cce29d8b534
Helen Strypko <elenkastrypko@gmail.com>
2018-04-22 18:22:08 +0300

    Added try catch statements for error

--- hard/task3-2.js
+++ hard/task3-2.js
@@ -2,14 +2,23 @@ function validate(arr,obj){
    for(var i=0; i<arr.length; i++){
        var key = arr[i]; 
            if(!(obj.hasOwnProperty(key))) {            
-                throw new Error("'age' wasn't passed on object");
+                throw new Error(key + " wasn't passed on object");
            } 
        }
        return true;
     }
 
+try{
+   console.log(validate(["name","surname"],{name:"Roman",surname:"Rodomansky"}));
+}
+catch(error){
+   console.log(error.message);
+}
 
-console.log(validate(["name","surname"],{name:"Roman",surname:"Rodomansky"}));
-console.log(validate(["name","surname","age"],{name:"Roman",surname:"Rodomansky"}));
-
+try{
+   console.log(validate(["name","surname","age"],{name:"Roman",surname:"Rodomansky"}));
+}
+catch(error){
+   console.log(error.message);
+}
 
