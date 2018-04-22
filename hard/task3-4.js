e8395ba5a1f76f4ac5b038a67e657b3b96ddc122
Helen Strypko <elenkastrypko@gmail.com>
2018-04-22 17:16:05 +0300

    added one more if statement

--- hard/task3-4.js
+++ hard/task3-4.js
@@ -1,4 +1,4 @@
-var array = [9, 9, 12, 2, 33, 4, 4, 5, 81, 10, 12];
+var array = [ 5, 5, 5, 5, 5, 5];
 console.log("array: " + array);
 
 var sorted_array = array.sort(); 
@@ -8,7 +8,9 @@ var results = [];
 function duplicate(array){
     for (var i = 0; i < array.length - 1; i++) {
         if (sorted_array[i + 1] == sorted_array[i]) {
-            results.push(sorted_array[i]);
+             if(!(results.includes(sorted_array[i]))){
+                    results.push(sorted_array[i]);
+                }
         }
     }
     return results;
