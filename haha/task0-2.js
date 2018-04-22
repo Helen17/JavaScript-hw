e515484cbbe7d12e720670dc6778df4a1735b2e8
Helen Strypko <elenkastrypko@gmail.com>
2018-04-22 16:58:22 +0300

    Added Event Listener for button "start"

--- haha/task0-2.js
+++ haha/task0-2.js
@@ -1,13 +1,14 @@
+document.getElementById("start").addEventListener("click", function(){
+var a = prompt("Please enter a:");
+var b = prompt("Please enter b:");
+    
 function pythagorean(a,b){
     var square = a*a + b*b;
     var c = Math.sqrt(square);
     return c;
 }
 
-var a = prompt("Please enter a:");
-var b = prompt("Please enter b:");
-
 alert('Result is: ' + pythagorean(a,b));
-
+});
 
 
